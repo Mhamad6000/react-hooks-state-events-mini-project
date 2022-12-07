d3f2b26db06b52be8f30338ab6fac68bf2e1cb84
Mhamad6000 <mhamadothman6000@gmail.com>
2022-12-07 23:53:11 +0300

    done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
-import React from "react";
-
-function TaskList() {
+import React, { useState } from "react";
+import Task from "./Task.js";
+function TaskList({ tasks, setTasksList }) {
+  function removeTask(taskToRemove) {
+    setTasksList(tasks.filter((task) => task.text !== taskToRemove.text));
+  }
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks !== undefined &&
+        tasks.map((task) => {
+          return <Task task={task} key={task.text} removeTask={removeTask} />;
+        })}
     </div>
   );
 }
